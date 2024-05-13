9708743e912e6ae1c0c9a510a4ba88b1005f2bf1
Sachintha Rashan <91013948+SACHIBOT@users.noreply.github.com>
2024-05-13 18:53:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 //index.js
 const EventEmitter = require('events');
 const TelegramBot = require('node-telegram-bot-api');
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 4000;
 
 process.env.NTBA_FIX_350 = true;
 
